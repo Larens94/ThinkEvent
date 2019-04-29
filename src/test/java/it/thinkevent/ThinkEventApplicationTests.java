1d20decbb93fb76e82313fbd911be3a15ef5d0fa
unknown <=>
2019-04-29 10:52:20 +0200

    first commit

--- src/test/java/it/thinkevent/ThinkEventApplicationTests.java
+++ src/test/java/it/thinkevent/ThinkEventApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StartCmsApplicationTests {
+public class ThinkEventApplicationTests {
 
 	@Test
 	public void contextLoads() {
